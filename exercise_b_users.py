a37a5aee09b5bb58cad9e236e53e13d8c4601c75
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-07-26 17:36:38 +0100

    finished up to question 7

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -63,9 +63,14 @@ print(users["Erik"]["lottery_numbers"])
 # 4. Get the species of Avril's pet Monty
 print(users["Avril"]["pets"][0]["species"])
 # 5. Get the smallest of Erik's lottery numbers
-
+smallest_lottery = min(users["Erik"]["lottery_numbers"])
+print(smallest_lottery)
 # 6. Return an list of Avril's lottery numbers that are even
+evenNumbers = lambda(users["Avril"]["lottery_numbers"])
+print(evenNumbers)
+# can't get this one
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+
 # 8. Change Erik's hometown to Edinburgh
 # 9. Add a pet dog to Erik called "fluffy"
 # 10. Add another person to the users dictionary
